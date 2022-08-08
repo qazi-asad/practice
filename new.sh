00e9156550d4b053ceb74501ecdd5fc815e34c5d
qazi-ejaz <qaziejazurrehman@gmail.com>
2022-08-08 23:40:06 +0100

    Added extra loop and comments in new.sh

--- new.sh
+++ new.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-for i in {a..z}
+# This script runs basic loop
+for i in {a..f}
 do
 echo $i
 done
 
+# I have added another loop for demonstration purpose
+for ((i=1; i<3; i++)); do
+echo "Outer Loop iteration: $i"
+done
