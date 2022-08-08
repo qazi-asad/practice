30349c55756f45c3c794e0777c021bfb3a54ef26
qazi-asad <qaziasad97@gmail.com>
2022-08-09 03:48:04 +0500

    Inner Loop is added

--- new.sh
+++ new.sh
@@ -9,4 +9,7 @@ done
 # I have added another loop for demonstration purpose
 for ((i=1; i<3; i++)); do
 echo "Outer Loop iteration: $i"
+for ((j=2; j<5; j++)); do
+echo "Inner Loop iteration: $j"
+done
 done
