8dd186424242a83268247550fef39a00d6d89d93
qazi-asad <qaziasad97@gmail.com>
2022-08-09 04:20:37 +0500

    While is practised

--- prac.sh
+++ prac.sh
@@ -1,7 +1,26 @@
-
+#!/bin/bash
 
 # Repeat all the tasks but use while loop instead of for loop
+i=1
+while ((i<3)); do
+echo "Outer Loop iteration: $i"
+j=2
+while ((j<5)); do
+echo "Inner Loop iteration: $j"
+((j++))
+done
+((i++))
+done
 
+arr=(Alhpa Bravo Charlie Delta Echo Foxtrot)
 
+# Write a for loop to print the elements of the array along with index
+### Your code gors here ###
+
+n=0
+while ((n < ${#arr[@]})); do
+echo "Index is: $n and element is ${arr[n]}"
+((n++))
+done
 
 
