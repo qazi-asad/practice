5f2a6b7ffc3ff281942ce1e2d3416b20f1e94f63
qazi-asad <qaziasad97@gmail.com>
2022-08-14 22:34:37 +0500

    New file is added regarding OPTARG

--- providing_options.sh
+++ providing_options.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-## Create a function named process_options that only accepts a,b, and c as options.
+## Create a function named process_option that only accepts a,b, and c as option.
 
+function process_options
+{
+    case $option in
+    a) echo "Selected option is $option";;
+    b) echo "Selected option is $option with argument $((OPTARG+4)) and OPTIND IS $OPTIND";;
+    c) echo "Selected option is $option with a argument $((OPTARG*4)) and OPTIND IS $OPTIND";;
+    *) echo "Please insert a, b or c";;
+    esac
+}
 
+## Create a user option in bash
 
-## Create a user options in bash
+while getopts "ab:c:d" option
+do 
+OPTARG=$((OPTARG-2))
+process_options
+done
